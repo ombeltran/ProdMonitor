0a29020b2f07cdf37dc441ae75cd3b430187000f
ombeltran <ombeltranduk@gmail.com>
2023-12-02 12:54:30 -0600

    fixed deploy XXII

--- front_end/src/api/axios.js
+++ front_end/src/api/axios.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:4000/api';
 
 const client = axios.create({
-    baseURL,
-    withCredentials: true
+    baseURL: baseURL,
+    withCredentials: true,
 });
 
 export default client;
