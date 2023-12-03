d321bb59087b98c6a08377030e73086e61f5386f
ombeltran <ombeltranduk@gmail.com>
2023-12-03 17:38:25 -0600

    fixed deploy XXVII

--- front_end/src/api/axios.js
+++ front_end/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_BASE_URL || 'https://backend-prodmonitor.onrender.com/api'; //http://localhost:4000/api'
+const baseURL = process.env.REACT_APP_BASE_URL || '//backend-prodmonitor.onrender.com/api'; //http://localhost:4000/api'
 
 const client = axios.create({
     baseURL: baseURL,
